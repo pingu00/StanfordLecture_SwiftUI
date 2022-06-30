0c17f7c0294e00ea20ec7e20b8a5322ff604ab46
pingu00 <abc@Phils-MacBook-Pro.local>
2022-07-01 02:00:20 +0900

    Model, ViewModel file generate
    앱을 MVVM 아키텍쳐에 맞는 패러다임으로 디자인 하는과정중, 모델과 뷰모델 제작

---
+++ Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,26 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by Phil on 2022/06/30.
+//
+
+import SwiftUI
+
+
+
+class EmojiMemoryGame { // no inheritance, because it's superclass
+    static let emojis:[String] = ["🚗","🚕","🚙","🚌","🚎","🏎","🚛","🚆","🚑","🚒","🚐","🛻","🚚","🚜","🚔","🚖"]
+    
+    static func createMemoryGame() -> MemoryGame<String> {
+        MemoryGame<String>(numberOfPairsOfCards: 4){ pairIndex in
+            EmojiMemoryGame.emojis[pairIndex]
+        }
+    }
+    private var model: MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
+                 
+    var cards: Array<MemoryGame<String>.Card> {
+        return model.cards
+    }
+    
+}
